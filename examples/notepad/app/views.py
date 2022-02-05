af0886f302ae6520216393311a8137ab6577e579
Payton Garland <payton.r.g@gmail.com>
2022-02-05 17:56:45 -0500

    Require CSRF cookie on index view in notepad example

--- examples/notepad/app/views.py
+++ examples/notepad/app/views.py
@@ -2,13 +2,14 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 
 import json
 
 from .models import Notepad, SharedNotepad
 
 
+@ensure_csrf_cookie
 def index(request):
     return render(request, "app/index.html")
 
