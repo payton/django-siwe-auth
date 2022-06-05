d6f00743d6b474f6c2bfbfb0c57fb479da1e9661
Payton Garland <payton.r.g@gmail.com>
2022-06-05 15:13:53 -0400

    Factor out endpoints in test file and add comments

--- tests/test_siwe_auth.py
+++ tests/test_siwe_auth.py
@@ -22,11 +22,13 @@ with open(path.join(BASE_TESTS, "validation_negative.json"), "r") as f:
 with open(path.join(BASE_TESTS, "validation_positive.json"), "r") as f:
     validation_positive = decamelize(json.load(fp=f))
 
+NONCE_ENDPOINT = '/api/auth/nonce'
+LOGIN_ENDPOINT = '/api/auth/login'
 
 class ApiTests(TestCase):
 
     def test_nonce(self):
-        nonce_response = self.client.get('/api/auth/nonce')
+        nonce_response = self.client.get(NONCE_ENDPOINT)
         self.assertEqual(nonce_response.status_code, 200)
         nonce_content = json.loads(nonce_response.content)
         self.assertTrue('nonce' in nonce_content)
@@ -39,21 +41,23 @@ class SigningClientApiTest(TestCase):
         self.account = Account.create()
 
     def getNonce(self):
-        nonce_response = self.client.get('/api/auth/nonce')
+        nonce_response = self.client.get(NONCE_ENDPOINT)
         nonce_content = json.loads(nonce_response.content)
         return nonce_content['nonce']
 
     def test_fails_without_nonce(self):
         for test_name, test in parsing_positive.items():
-            message = SiweMessage(test["fields"])
+            message = SiweMessage(test['fields'])
             message.address = self.account.address
 
+            # Convert SIWE message into dictionary for body of login HTTP request
             message_obj = {}
             for slot in message.__slots__:
                 slotvalue = getattr(message, slot, None)
                 if slotvalue is not None:
                     message_obj[slot] = slotvalue
 
+            # Sign SIWE message with test Ethereum account
             signature = self.account.sign_message(
                 messages.encode_defunct(text=message.prepare_message())
             ).signature
@@ -62,27 +66,31 @@ class SigningClientApiTest(TestCase):
                 'content_type': 'application/json',
             }
 
+            # Assert that missing nonce fails login request
             sig = signature.hex()
             with self.assertRaises(Nonce.DoesNotExist):
-                loginAttemptResponse = self.client.post("/api/auth/login", {
+                loginAttemptResponse = self.client.post(LOGIN_ENDPOINT, {
                     'message': message_obj,
                     'signature': sig
                 }, **headers)
 
     def test_message_round_trip(self):
         for test_name, test in parsing_positive.items():
-            message = SiweMessage(test["fields"])
+            message = SiweMessage(test['fields'])
             message.address = self.account.address
 
+            # Add nonce to SIWE message
             nonce = self.getNonce()
             message.nonce = nonce
 
+            # Convert SIWE message into dictionary for body of login HTTP request
             message_obj = {}
             for slot in message.__slots__:
                 slotvalue = getattr(message, slot, None)
                 if slotvalue is not None:
                     message_obj[slot] = slotvalue
 
+            # Sign SIWE message with test Ethereum account
             signature = self.account.sign_message(
                 messages.encode_defunct(text=message.prepare_message())
             ).signature
@@ -91,8 +99,9 @@ class SigningClientApiTest(TestCase):
                 'content_type': 'application/json',
             }
 
+            # Assert that login request with valid nonce returns a 200
             sig = signature.hex()
-            loginAttemptResponse = self.client.post("/api/auth/login", {
+            loginAttemptResponse = self.client.post(LOGIN_ENDPOINT, {
                 'message': message_obj,
                 'signature': sig
             }, **headers)
